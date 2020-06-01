fe2721b3b808a33888938b963b47a74dfb2b6cae
Joan Nekoye <joannekoye@gmail.com>
2020-06-01 10:26:58 +0300

    create index view and html

--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
-from .models import
-from .forms import 
+from .models import Image, Comment, Profile, Likes
+# from .forms import 
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
+
+def index(request):
+    '''
+    view function to display landing page
+    '''
+
+    return render(request, 'index.html')
