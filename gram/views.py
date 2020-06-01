77101ff5990a05a96b17d5775f0df6b10d40a40e
Joan Nekoye <joannekoye@gmail.com>
2020-06-01 09:54:30 +0300

    set up views

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+import datetime as dt
+from .models import
+from .forms import 
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
