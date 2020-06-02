2a4bad9a335bad45734b0b3fecab6542579560ff
Joan Nekoye <joannekoye@gmail.com>
2020-06-02 07:44:43 +0300

    finish creating views

--- gram/urls.py
+++ gram/urls.py
@@ -5,4 +5,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name = 'home'),
+    path('upload_image/', views.image, name = 'image'),
+    path('accounts/profile/', views.profile, name = 'profile'),
+    path('profile/', views.otherprofile, name = 'otherprofile'),
+    path('search/', views.search,name="search"),
+    path('profile/update/', views.update,name='update'),
+    path('comment/', views.comment,name='comment'),
+    path('like/', views.like,name='like'),
+    path('follow/', views.follow,name='follow'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
