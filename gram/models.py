d0f6c3b8bb8193ce6a2af95075ca3ffd73b47f13
Joan Nekoye <joannekoye@gmail.com>
2020-06-02 03:42:38 +0300

    Update models

--- gram/models.py
+++ gram/models.py
@@ -4,39 +4,59 @@ from tinymce.models import HTMLField
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    bio = models.TextField(max_length = 30, blank = True)
-    pic = models.ImageField(upload_to = 'images/', null =True, blank = True)
-    followers = models.ManyToManyField('Profile', related_name = 'followers_profile', blank =True)
-    following = models.ManyToManyField('Profile', related_name='following_profile', blank =True)
+    main_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    bio = models.TextField(max_length = 300, blank = True, default='')
+    pic = models.ImageField(upload_to = 'images/', null =True, blank = True, default='')
+    followers = models.CharField(max_length = 30, blank = True, default = 0)
+    following = models.CharField(max_length = 30, blank = True, default = 0)
 
     def __str__(self):
-        return self.user.username
+        return self.main_user.username
+
+    @classmethod
+    def search_users(cls,name):
+        users=cls.objects.filter(main_user__username__icontains=name)
+        return users
+
+    @classmethod
+    def get_profile(cls,user):
+        profile=cls.objects.get(main_user=user)
+        return profile
 
 class Comment(models.Model):
     comment = models.TextField(max_length = 30)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    main_user = models.ForeignKey(User,on_delete=models.CASCADE)
     upload_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.comment
 
 class Like(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    picture = models.CharField(max_length = 30, default = '')
+    main_user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.main_user.username
 
 class Image(models.Model):
     image_path = models.ImageField(upload_to = 'images/')
-    name = models.CharField(max_length = 50)
+    main_user = models.ForeignKey(User, on_delete = models.CASCADE)
     caption = models.TextField(blank = True)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     upload_date = models.DateTimeField(auto_now_add=True)
-    likes = models.ForeignKey(Likes,on_delete=models.CASCADE)
-    comments = models.ManyToManyField(Comment)
+    likes = models.CharField(max_length = 30, blank = True, default = 0)
+    comments = models.CharField(max_length = 30, blank = True, default = 0)
 
     def __str__(self):
         return self.name
+        return self.caption
 
-Comment.image = models.ForeignKey(Image, on_delete=models.CASCADE)
-Likes.image = models.ForeignKey(Image, on_delete=models.CASCADE)
+class Contact(models.Model):
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
+    user_to = models.ForeignKey(User, related_name='rel_to_set')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to )
 
 
